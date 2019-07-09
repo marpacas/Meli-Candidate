3d1be29e715e4fd0d722092b2eeefc34582573d8
Martha Castillo <martha@mobilenik.com>
2019-07-08 23:40:43 -0300

    Include Service Tests
    - Update SearchItemsServiceTests

--- MeliCandidateTests/Service_Tests/SearchItemsTests.m
+++ MeliCandidateTests/Service_Tests/SearchItemsTests.m
@@ -7,8 +7,22 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "MLCSearchItemsService.h"
+#import "MLCConstants.h"
+
+@interface MLCSearchItemsService ()
+
+@property(nonatomic,strong,nonnull)NSURLSessionDataTask* getItemsListTask;
+
+@end
 
 @interface SearchItemsTests : XCTestCase
+
+@property(nonatomic, strong) NSString* textToSearch;
+@property(nonatomic, strong) NSString* validEndPoint;
+@property(nonatomic, strong) NSString* invalidEndPoint;
+
+@property(nonatomic, strong)MLCSearchItemsService* searchItemsService;
 
 @end
 
@@ -16,24 +30,97 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.textToSearch = @"Disco externo";
+    self.validEndPoint = @"/sites/MLA/search";
+    self.invalidEndPoint = @"/site/MLA/search";
+    self.searchItemsService = [[MLCSearchItemsService alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    self.searchItemsService = nil;
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+- (void)testSearchItemsWhenEverythingIsAllRight {
+    [self.searchItemsService.getItemsListTask cancel];
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+    NSString* endpoint = [kBaseURL stringByAppendingString:self.validEndPoint];
+    NSURLComponents* URLComponents =[NSURLComponents componentsWithString:endpoint];
+    URLComponents.queryItems = @[[NSURLQueryItem queryItemWithName:@"q" value:self.textToSearch]];
+    NSString *description = [NSString stringWithFormat:@"GET %@", endpoint];
+    XCTestExpectation *expectation = [self expectationWithDescription:description];
+    if (!self.searchItemsService.session) {
+        self.searchItemsService.session = [NSURLSession sharedSession];
+    }
+    self.searchItemsService.getItemsListTask = [self.searchItemsService.session dataTaskWithURL:URLComponents.URL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        XCTAssertNotNil(data, "Data should not be nil in Search Items Service");
+        XCTAssertNil(error, "Error should be nil in Search Items Service");
+        
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            XCTAssertEqual(httpResponse.statusCode, 200, @"HTTP response status code should be 200 in Search Items");
+            XCTAssertEqualObjects(httpResponse.URL.absoluteString, URLComponents.URL.absoluteString, @"HTTP response URL should be equal to original URL in Search Items Service");
+            XCTAssertEqualObjects(httpResponse.MIMEType, @"application/json", @"HTTP response content type should be application/json in Search Items Service");
+        }
+        else {
+            XCTFail(@"Response was not NSHTTPURLResponse in Search Items Service");
+        }
+        
+        [expectation fulfill];
+
     }];
+    [self.searchItemsService.getItemsListTask resume];
+    
+    [self waitForExpectationsWithTimeout:self.searchItemsService.getItemsListTask.originalRequest.timeoutInterval handler:^(NSError *error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        }
+        [self.searchItemsService.getItemsListTask cancel];
+    }];
+    
+
 }
+
+-(void)testSearchItemsWhenBadRequest {
+    [self.searchItemsService.getItemsListTask cancel];
+    
+    NSString* endpoint = [kBaseURL stringByAppendingString:self.invalidEndPoint];
+    NSURLComponents* URLComponents =[NSURLComponents componentsWithString:endpoint];
+    URLComponents.queryItems = @[[NSURLQueryItem queryItemWithName:@"q" value:self.textToSearch]];
+    NSString *description = [NSString stringWithFormat:@"GET %@", endpoint];
+    XCTestExpectation *expectation = [self expectationWithDescription:description];
+    if (!self.searchItemsService.session) {
+        self.searchItemsService.session = [NSURLSession sharedSession];
+    }
+    self.searchItemsService.getItemsListTask = [self.searchItemsService.session dataTaskWithURL:URLComponents.URL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        XCTAssertNotNil(data, "Data should not be nil in Search Items Service when bad request");
+        XCTAssertNil(error, "Error should not nil in Search Items Service when bad request");
+        
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            XCTAssertNotEqual(httpResponse.statusCode, 200, @"HTTP response status code should not be 200 in Search Items");
+            XCTAssertEqualObjects(httpResponse.URL.absoluteString, URLComponents.URL.absoluteString, @"HTTP response URL should be equal to original URL in Search Items Service");
+            XCTAssertEqualObjects(httpResponse.MIMEType, @"application/json", @"HTTP response content type should be application/json in Search Items Service");
+
+        }
+        else {
+            XCTFail(@"Response was not NSHTTPURLResponse in Search Items Service");
+        }
+        
+        [expectation fulfill];
+        
+    }];
+    [self.searchItemsService.getItemsListTask resume];
+    
+    [self waitForExpectationsWithTimeout:self.searchItemsService.getItemsListTask.originalRequest.timeoutInterval handler:^(NSError *error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        }
+        [self.searchItemsService.getItemsListTask cancel];
+    }];
+
+    
+}
+
 
 @end
